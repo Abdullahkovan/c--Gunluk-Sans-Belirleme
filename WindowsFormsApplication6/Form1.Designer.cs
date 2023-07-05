0344e3a06efd9dc09390be6cbeef9183811d37c1
Abdullahkovan <138412345+Abdullahkovan@users.noreply.github.com>
2023-07-06 02:46:43 +0300

    c# Gunluk Sans Belirleme
    c# Gunluk Sans Belirleme Programi

---
+++ WindowsFormsApplication6/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace WindowsFormsApplication6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.ForeColor = System.Drawing.Color.SeaGreen;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 283);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(170, 283);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 95);
+            this.listBox2.TabIndex = 1;
+            // 
+            // listBox3
+            // 
+            this.listBox3.ForeColor = System.Drawing.Color.OrangeRed;
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(323, 283);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(120, 95);
+            this.listBox3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.ForeColor = System.Drawing.Color.Transparent;
+            this.button1.Location = new System.Drawing.Point(79, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 133);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "ŞANSINI DENE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Coral;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Location = new System.Drawing.Point(288, 74);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(72, 48);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "ÇIKIŞ YAP";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(167, 218);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "PUANINIZ:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Green;
+            this.label2.Location = new System.Drawing.Point(246, 218);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Aqua;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.Location = new System.Drawing.Point(424, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(28, 21);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "?";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Maroon;
+            this.label3.Location = new System.Drawing.Point(12, 264);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "DOĞRU BİLDİKLERİNİZ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Maroon;
+            this.label4.Location = new System.Drawing.Point(176, 264);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "TAHMİNLERİNİZ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Maroon;
+            this.label5.Location = new System.Drawing.Point(331, 264);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "RANDOM SAYILAR";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(462, 403);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "BUGÜN NE KADAR ŞANSLISIN BAKALIM!!";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WindowsFormsApplication6/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace WindowsFormsApplication6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                {
+
+                    listBox1.Items.Clear();
+                    listBox2.Items.Clear();
+                    listBox3.Items.Clear();
+                    
+                    int bas = 1, bit = 80;
+                    int[] girilensayılar = new int[10];
+                    int y = 0;
+                    MessageBox.Show("Unutma 1 İle 80 Arasında 10 tane Sayı Giriceksin");
+                    while (y < 10)
+
+                    {
+                        int tahminsayısı = int.Parse(Interaction.InputBox("1-80 arasında 10 tane sayı giriniz(Öğrenmek için soru işaretine tıklayınız)", "Sayınızı Giriniz", "0", 250, 250));
+                        listBox2.Items.Add(tahminsayısı);
+                        
+                        if (tahminsayısı < bas || tahminsayısı > bit)
+                        {
+                            MessageBox.Show("1-80 arasında dedim!!");
+                            continue;
+                        }
+
+                        {
+                            int yoksa = Array.IndexOf(girilensayılar, tahminsayısı);
+                            if (yoksa == -1) // AYNI SAYIYI GİRMEZSE DÖNGÜ DEVAM EDİYOR 
+                            {
+
+                                girilensayılar[y] = tahminsayısı;
+                                y++;
+                            }
+                            else   // AYNI SAYIYI GİRERSE TEKRAR BAŞA DÖNÜP SORUYOR 
+                            {
+                                MessageBox.Show("AYNI SAYIYI GİREMESSİN !!");
+                                MessageBox.Show("BU HATAYI 2-3 KERE ALDIYSAN BAŞKA BİRİNE VER KLAVYE MAUSU!!");
+                            }
+                        }
+                        
+                    } listBox2.Visible = true;
+                    label3.Visible = true;
+                    label4.Visible = true;
+                    label5.Visible = true;
+
+                    Random rastgelesayı = new Random();
+                    int[] randomsayılarım = new int[20];
+                    int x = 0;
+                    while (x < 20)
+                    {
+                        int şanslısayı = rastgelesayı.Next(bas, bit + 1);
+                        int yoksa = Array.IndexOf(randomsayılarım, şanslısayı);
+                        if (yoksa == -1)
+                        {
+                            randomsayılarım[x] = şanslısayı;
+                            listBox3.Items.Add(randomsayılarım[x]);
+                            listBox3.Visible = true;
+                            x++;
+                        }
+
+
+                    }
+                    foreach (int girilen in girilensayılar)
+                    {
+                        foreach (int randomlar in randomsayılarım)
+                        {
+                            if (randomlar == girilen)
+                            {
+                                listBox1.Visible = true;
+                                listBox1.Items.Add("Doğru Bildin " + randomlar + "=" + girilen);
+
+                            }
+                           
+                        }
+                    }
+                    int puanlama = listBox1.Items.Count * 10;
+                    if (puanlama==0)
+                    {
+                        MessageBox.Show("SIĞINAĞIN VARSA ORADA BEKLE");
+                        MessageBox.Show("Puanınız " + puanlama);
+                        label2.Text = puanlama.ToString();
+                        label1.Visible = true;
+                        label2.Visible = true;
+                    }
+                    if (puanlama == 10)
+                    {
+                        MessageBox.Show("BUGÜN SADECE YATAKTA KAL");
+                        MessageBox.Show("Puanınız " + puanlama);
+                        label2.Text = puanlama.ToString();
+                        label1.Visible = true;
+                        label2.Visible = true;
+                    }
+                    else if (puanlama == 20)
+                    {
+                        MessageBox.Show("BUGÜN PRİZE KABLO BİLE TAKMA ");
+                        MessageBox.Show("Puanınız " + puanlama);
+                        label2.Text = puanlama.ToString();
+                        label1.Visible = true;
+                        label2.Visible = true;
+                    }
+                    else if (puanlama == 30)
+                    {
+                        MessageBox.Show("EVDEN BİLE CIKMA DERİM");
+                        MessageBox.Show("Puanınız " + puanlama);
+                        label2.Text = puanlama.ToString();
+                        label1.Visible = true;
+                        label2.Visible = true;
+                    }
+                    else if (puanlama == 40)
+                    {
+                        MessageBox.Show("KENDİNİ FAZLA TEHLİKEYE ATMA HAYATINA DEVAM ET");
+                        MessageBox.Show("Puanınız " + puanlama);
+                        label2.Text = puanlama.ToString();
+                        label1.Visible = true;
+                        label2.Visible = true;
+                    }
+                    else if (puanlama == 50)
+                    {
+                        MessageBox.Show("NORMAL YAŞAMINA DEVAM ET");
+                        MessageBox.Show("Puanınız " + puanlama);
+                        label2.Text = puanlama.ToString();
+                        label1.Visible = true;
+                        label2.Visible = true;
+                    }
+                    else if (puanlama == 60)
+                    {
+                        MessageBox.Show("ÇOK AZ ŞANSLISIN BUGÜN ŞANSLA İLGİLİ BİŞEY YAPMA");
+                        MessageBox.Show("Puanınız " + puanlama);
+                        label2.Text = puanlama.ToString();
+                        label1.Visible = true;
+                        label2.Visible = true;
+                    }
+                    else if (puanlama == 70)
+                    {
+                        MessageBox.Show("70 PUAN İYİ BELKİ 1-2 DEFA KAZI KAZAN YAPABİLİRSİN");
+                        MessageBox.Show("Puanınız " + puanlama);
+                        label2.Text = puanlama.ToString();
+                        label1.Visible = true;
+                        label2.Visible = true;
+                    }
+                    else if (puanlama == 80)
+                    {
+                        MessageBox.Show("ŞANSIN GAYET İYİ BALIĞA FİLAN GİT GÜZEL BALIKLAR TUTARSIN");
+                        MessageBox.Show("Puanınız " + puanlama);
+                        label2.Text = puanlama.ToString();
+                        label1.Visible = true;
+                        label2.Visible = true;
+                    }
+                    else if (puanlama == 90)
+                    {
+                        MessageBox.Show("EVLİ DEĞİLSEN VEYA SEVGİLİN YOKSA BUGÜN ŞANSINI DENE DERİM");
+                        MessageBox.Show("Puanınız " + puanlama);
+                        label2.Text = puanlama.ToString();
+                        label1.Visible = true;
+                        label2.Visible = true;
+                    }
+                    else if (puanlama == 100)
+                    {
+                        MessageBox.Show("KOŞ DİREK LOTO FİLAN OYNA");
+                        MessageBox.Show("Puanınız " + puanlama);
+                        label2.Text = puanlama.ToString();
+                        label1.Visible = true;
+                        label2.Visible = true;
+                    }
+                }
+
+            }
+            catch (FormatException)  // Eğer harf girerse bu uyarıyı veriyor
+            {
+                MessageBox.Show("Lütfen Sayı Giriniz(harf veya noktalama işaretleri girme)");
+
+            }
+
+            catch   // genel hata yeri
+            {
+                MessageBox.Show("Hata var ama nedeni belli değil");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            {
+                listBox1.Visible = false;
+                listBox2.Visible = false;
+                listBox3.Visible = false;
+                label1.Visible = false;
+                label2.Visible = false;
+                label3.Visible = false;
+                label4.Visible = false;
+                label5.Visible = false;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            MessageBox.Show("1-80 arasında bilgisayar 20 tane random sayı alır. Sizin 1-80 arasında gireceğiniz 10 tane aynı olmayan sayıyla karşılaştırıp aynı olan sayılara puan verir. Oynun sonunda puanınızı gösterir.", "BİLGİLENDİRME", MessageBoxButtons.OK,MessageBoxIcon.Information);
+            
+        }
+    }
+        }
+
+        
+            
